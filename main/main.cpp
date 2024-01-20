9b3c8d53847ded054e5d4a3c341e3416fa979996
Mechatronics <>
2024-01-19 17:56:44 -0800

    Fix Memory Allocation

--- main/main.cpp
+++ main/main.cpp
@@ -9,6 +9,7 @@
 #define READ_BYTES_ERROR(bytes) bytes<0
 #define FIRST_BYTE(frame_data) frame_data[0]
 
+constexpr const char* ROBOT_PROGRAM             =  "/";
 constexpr const char* READ_ERROR_MESSAGE        =  "Error when reading bytes";
 constexpr const char* ROBOT_STARTING_MESSAGE    =  "Button press signal received! Starting robot...\n";
 constexpr const int   SUB_STATE_SIGNAL_ID       =   0x07;    
@@ -16,7 +17,9 @@ constexpr const int   START_CODE                =   0x04;
 
 bool received_start_command(CAN_Setup_Info& setup_info)
 {
-    return setup_info.frame.can_id == SUB_STATE_SIGNAL_ID && FIRST_BYTE(setup_info.frame.data) == START_CODE;
+    const bool is_sub_state_msg = setup_info.frame.can_id == SUB_STATE_SIGNAL_ID;
+    const bool is_start_code = FIRST_BYTE(setup_info.frame.data) == START_CODE;
+    return is_sub_state_msg && is_start_code;
 }
 
 int main(int argc, char * argv[])
@@ -31,7 +34,7 @@ int main(int argc, char * argv[])
 
         if (received_start_command(setup_info)) {
             printf(ROBOT_STARTING_MESSAGE);
-            startTargetProgram("/");
+            startTargetProgram(ROBOT_PROGRAM);
         }
     }
     exit(EXIT_SUCCESS);
--- src/can_button.cpp
+++ src/can_button.cpp
@@ -8,17 +8,15 @@ constexpr const char* CAN_INTERFACE     = "can0";
 
 CAN_Setup_Info setupCan()
 {
-    int sock;
+    int* sock = new int();
     struct sockaddr_can addr;
     struct ifreq ifr;
-    sock = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    *sock = socket(PF_CAN, SOCK_RAW, CAN_RAW);
     strcpy(ifr.ifr_name, CAN_INTERFACE);
-    ioctl(sock, SIOCGIFINDEX, &ifr);
+    ioctl(*sock, SIOCGIFINDEX, &ifr);
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
-    bind(sock, (struct sockaddr *)&addr, sizeof(addr));
-    struct can_frame frame;
-    return CAN_Setup_Info{sock, frame};
+    bind(*sock, (struct sockaddr *)&addr, sizeof(addr));
+    struct can_frame* frame = new can_frame();
+    return CAN_Setup_Info{*sock, *frame};
 }
-
-
